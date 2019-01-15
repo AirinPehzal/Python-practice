08ef7b0b62d6b8e4e7adeba3478c82aa3e6757e2
AirinPehzal <44808834+AirinPehzal@users.noreply.github.com>
2019-01-15 17:16:03 +0300

    Delete program3.py

--- program3.py
+++
@@ -1,7 +0,0 @@
-word = input("Введите слово (в кириллице)\n")
-if (word[0] == 'а') or (word[0] == 'э') or (word[0] == 'е') or (word[0] == 'я'):
-    for i in range(0, len(word), 1):
-        print(word[i])
-else:
-    for i in range(len(word)-1, -1, -1):
-        print(word[i])
