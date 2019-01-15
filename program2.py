c1876648de7d9218e9ee83b695d9d9d4133b4e8b
AirinPehzal <44808834+AirinPehzal@users.noreply.github.com>
2019-01-15 17:06:20 +0300

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,7 @@
+word = input("Введите слово (в кириллице)\n")
+if (word[1] == 'к') or (word[1] == 'и') or (word[1] == 'л'):
+    for i in range(1, len(word), 2):
+        print(word[i])
+else:
+    for i in range(0, len(word), 2):
+        print(word[i])
